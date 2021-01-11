c3aa204b434212974e5afe691ef8e286714146ca
Danamorah <damoraherrera@gmail.com>
2021-01-11 16:03:53 -0300

    add server && update app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,7 @@
 import React, { useState } from 'react';
 
-export default function App() {
-  const [count, setCount] = useState(0);
+export default function App({title}) {
   return (
-    <div>
-      This is a sample stateful and server-side
-      rendered React application.
-      <br />
-      <br />
-      Here is a button that will track
-      how many times you click it:
-      <br />
-      <br />
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-    </div>
+    <div className="header">{title}</div>
   );
 }

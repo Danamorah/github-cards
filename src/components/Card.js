dcf8adda5d6bf9b3cc2321ec4f85dbc6bce84bac
Danamorah <damoraherrera@gmail.com>
2021-01-16 22:57:00 -0300

    ad props to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import testData from '../testData'
 
 
 class Card extends React.Component {
-
   render(){
-    const profile = testData[0]
+    const profile = this.props
     return(
       <div className="github-profile">
         <img src={profile.avatar_url}/>
         <div className="info" >
-          <div className="name">Name</div>
-          <div className="company">Company</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     ) 
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Card from './Card'
+import testData from '../testData'
 
 const CardList = () => {
   return(
     <div>
-      <Card/>
+      {testData.map(profile => <Card key={profile.name} {...profile}/>)}
+      {/* <Card {...testData[0]} /> */}
+      {/* when we use the spread operator in an object like this in the react component 
+        all te properties of that object will become props for this component */}
     </div>
   )
 }

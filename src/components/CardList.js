24e067fabeb5674c36db96d9dfb4a92138d06b85
Danamorah <damoraherrera@gmail.com>
2021-01-16 23:31:25 -0300

    back to index as a key, test purpose

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,8 +5,8 @@ import testData from '../testData'
 const CardList = () => {
   return(
     <div>
-      {testData.map((profile) =>
-        <Card key={profile.avatar_url} {...profile}/>)}
+      {testData.map((profile, index) =>
+        <Card key={index} {...profile}/>)}
       {/* <Card {...testData[0]} /> */}
       {/* when we use the spread operator in an object like this in the react component 
         all te properties of that object will become props for this component */}
